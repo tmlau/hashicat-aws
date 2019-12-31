1a560b0346054ad2d93008dc37c0d823e86e4d84
Jim Ray <jray@hashicorp.com>
2019-12-31 16:57:51 +0000

    added VPC module from PMR take 3

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,33 @@
-module "vpc" {
-  source  = "app.terraform.io/jray-hashi/vpc/aws"
-  version = "7"
+provider "aws" {
+  region = var.region
+}
 
-  cidr = "172.16.16.0/24"
-  name = "${prefix}-vpc"
+module "vpc" {
+  source = "app.terraform.io/jray-hashi/vpc/aws"
+
+  name = "simple-example"
+
+  cidr = "10.10.0.0/16"
+
+  azs             = ["${var.region}a"]
+  private_subnets = ["10.10.1.0/24"]
+  public_subnets  = ["10.10.101.0/24"]
+
+  enable_ipv6 = true
+
+  enable_nat_gateway = false
+  single_nat_gateway = false
+
+  public_subnet_tags = {
+    Name = "overridden-name-public"
+  }
+
+  tags = {
+    Owner       = "${var.prefix}-hashicorp"
+    Environment = "${var.prefix}-instruqt-workshop"
+  }
+
+  vpc_tags = {
+    Name = "vpc-name"
+  }
 }
